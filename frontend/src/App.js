ad795fe36d478218a34ce93af6353c0e2dd4a79f
juusingsing <ansjs1@naver.com>
2025-09-24 16:48:56 +0900

    [문주성] 테스트파일삭제

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
 import HeroSection from "./pages/HeroSection";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -12,9 +11,6 @@ import Sensor from "./pages/Sensor";
 import Video from "./pages/Video";
 
 import Option from "./pages/Option";
-
-import Page1 from "./pages/page1";
-import Page2 from "./pages/page2";
 
 import MainLayout from "./layouts/MainLayout";
 import TopBar from "../src/componants/TopBar";
@@ -37,8 +33,6 @@ function App() {
         <Route path="/option" element={<Option />} />
         <Route path="/video" element={<Video/>}/>
         </Route>
-        <Route path="/1" element={<Page1 />} />
-        <Route path="/2" element={<Page2 />} />
       </Routes>
     </Router>
   );
